03a0a78dc4e9bf90ba56b792055dcc0659602479
PixelGuy75 <crafterguy75@gmail.com>
2014-12-23 20:05:22 -0600

    Version 1.1
    Adds more executable commands on different events. Also new config.yml

--- src/PixelGuy75/JoinCommands/Main.php
+++ src/PixelGuy75/JoinCommands/Main.php
@@ -10,6 +10,9 @@ use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 
 class Main extends PluginBase implements Listener {
   public function onEnable() {
@@ -19,7 +22,25 @@ class Main extends PluginBase implements Listener {
   
   public function onJoin(PlayerJoinEvent $event) {
 	$player = $event->getPlayer();
-	$command = str_replace("{player}", $player->getName(), $this->getConfig()->get("Command"));
+	$command = str_replace("{player}", $player->getName(), $this->getConfig()->get("JoinCommand"));
+	$this->getServer()->dispatchCommand(new ConsoleCommandSender(), $command);
+  }
+  
+  public function onDeath(PlayerDeathEvent $event) {
+	$player = $event->getEntity();
+	$command = str_replace("{player}", $player->getName(), $this->getConfig()->get("DeathCommand"));
+	$this->getServer()->dispatchCommand(new ConsoleCommandSender(), $command);
+  }
+  
+  public function onRespawn(PlayerRespawnEvent $event) {
+	$player = $event->getPlayer();
+	$command = str_replace("{player}", $player->getName(), $this->getConfig()->get("RespawnCommand"));
+	$this->getServer()->dispatchCommand(new ConsoleCommandSender(), $command);
+  }
+  
+  public function onQuit(PlayerQuitEvent $event) {
+	$player = $event->getPlayer();
+	$command = str_replace("{player}", $player->getName(), $this->getConfig()->get("LeaveCommand"));
 	$this->getServer()->dispatchCommand(new ConsoleCommandSender(), $command);
   }
 }
